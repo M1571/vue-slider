7873d0cf381ee0c6f54f81a99d55c68078a45733
M1571 <94639639+M1571@users.noreply.github.com>
2022-02-24 14:09:09 +0000

    set evrth

---
+++ app.js
@@ -0,0 +1,15 @@
+// console.log('prova');
+
+/*
+Descrizione:
+Partendo dal markup della versione svolta vanilla js con l’array di oggetti,
+rifare lo slider ma questa volta usando Vue
+
+Bonus:
+1. al click su una thumb, visualizzare in grande l’immagine corrispondente
+2. applicare l’autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente 
+(attenzione! con Vue si fa in maniera leggermente diversa da come lo abbiamo fatto in vanilla-js)
+3. quando il mouse va in hover sullo slider, bloccare l’autoplay e farlo riprendere quando esce
+*/
+
+// -------------------------------------
