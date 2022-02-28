c176617b448e9764240fcd238f2fc14efc6c197a
M1571 <94639639+M1571@users.noreply.github.com>
2022-02-28 11:10:43 +0100

    ennesimo push se va

--- app.js
+++ app.js
@@ -1,128 +1,191 @@
-// console.log('prova');
-
-/*
-Descrizione:
-Partendo dal markup della versione svolta vanilla js con l’array di oggetti,
-rifare lo slider ma questa volta usando Vue
-
-Bonus:
-1. al click su una thumb, visualizzare in grande l’immagine corrispondente
-2. applicare l’autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente 
-(attenzione! con Vue si fa in maniera leggermente diversa da come lo abbiamo fatto in vanilla-js)
-3. quando il mouse va in hover sullo slider, bloccare l’autoplay e farlo riprendere quando esce
-*/
-
-// ----------------------------------------------
-
 /*
 const items = [
-    'img/01.jpg',
-    'img/02.jpg',
-    'img/03.jpg',
-    'img/04.jpg',
-    'img/05.jpg'
-];
-
-const title = [
-    'Svezia',
-    'Svizzera',
-    'Gran Bretagna',
-    'Germania',
-    'Paradise'
-];
-
-const text = [
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-    'Lorem ipsum',
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-];
-
-// ----------------------------------------------
-
-let title2 = document.querySelector(".img-bg h2")
-
-let active = 0;
-
-let imgBg = document.querySelector(".img-bg");
-
-let pargF = document.querySelector(".img-bg p");
-
-// ----------------------------------------------
-
-const btn = document.querySelector(".btn-dw");
-btn.addEventListener('click',
-    function () {
-        if (active < items.length - 1) {
-            console.log("click");
-            active++
-            console.log(active);
-            imgBg.setAttribute("src", items[active])
-            title2.innerHTML = title[active]
-            pargF.innerHTML = text[active]
-        }
+    {
+        photo: 'img/01.jpg',
+        title: "Svezia",
+        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis."
+    },
+    {
+        photo: 'img/02.jpg',
+        title: "Svizzera",
+        text: "Lorem ipsum."
+    },
+    {
+        photo: 'img/03.jpg',
+        title: "Gran Bretagna",
+        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit."
+    },
+    {
+        photo: 'img/04.jpg',
+        title: "Germania",
+        text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam."
+    },
+    {
+        photo: 'img/05.jpg',
+        title: "Paradise",
+        text: "Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam."
     }
-
-)
+];
 
 // ----------------------------------------------
 
-let btnUp = document.querySelector(".btn-up")
-btnUp.addEventListener('click',
-    function () {
-        if (active > 0) {
-            active--
-            imgBg.setAttribute("src", items[active])
-            title2.innerHTML = title[active]
-            pargF.innerHTML = text[active]
-        }
+const itemsRef = document.getElementsByClassName('items')[0];
+const thumbsRef = document.getElementsByClassName('thumbs')[0];
+
+let item = '';
+let thumb = '';
+let active = 1;
+
+// ----------------------------------------------
+
+for (let key in items) {
+    item += `
+        <div class="item">
+            <img src="${items[key].photo}" alt="">
+            <div class="text">
+                <h3>${items[key].title}</h3>
+                <p>${items[key].text}</p>
+            </div>
+        </div>`
+    thumb += `
+        <div class="thumb">
+            <img src="${items[key].photo}" alt="">
+        </div>
+    `
+}
+
+// ----------------------------------------------
+
+itemsRef.innerHTML = item;
+document.getElementsByClassName('item')[active].classList.add('active');
+
+thumbsRef.innerHTML += thumb;
+document.getElementsByClassName('thumb')[active].classList.add('active');
+
+// ----------------------------------------------
+
+const prev = document.querySelector('.prev');
+prev.addEventListener('click', function() {
+    if(active == 0) {                                                                   
+        active = items.length - 1; 
+
+        document.querySelector('.item.active').classList.remove('active');
+        document.getElementsByClassName('item')[active].classList.add('active');
+
+        document.querySelector('.thumb.active').classList.remove('active');
+        document.getElementsByClassName('thumb')[active].classList.add('active');
+    } else if(active < items.length) {
+        --active
+        document.querySelector('.item.active').classList.remove('active');
+        document.getElementsByClassName('item')[active].classList.add('active');
+
+        document.querySelector('.thumb.active').classList.remove('active');
+        document.getElementsByClassName('thumb')[active].classList.add('active');
+    } 
+});
+
+// ----------------------------------------------
+
+const next = document.querySelector('.next');
+next.addEventListener('click', function() {
+    if(active < items.length - 1) {
+        ++active
+        document.querySelector('.item.active').classList.remove('active');
+        document.getElementsByClassName('item')[active].classList.add('active');
+
+        document.querySelector('.thumb.active').classList.remove('active');
+        document.getElementsByClassName('thumb')[active].classList.add('active');
+    } else if(active == items.length - 1) { 
+        active = 0;
+        document.querySelector('.item.active').classList.remove('active');
+        document.getElementsByClassName('item')[active].classList.add('active');
+
+        document.querySelector('.thumb.active').classList.remove('active');
+        document.getElementsByClassName('thumb')[active].classList.add('active');
     }
-)
+});
 */
 
 // ----------------------------------------------
+// VUE
 
-const container = new Vue(
+const app = new Vue(
     {
-        el: '#main-container',
-    
-        carousel: [
+        el: '#app',
+        data: {
+            items: [
+                {
+                    url: './img/01.jpg',
+                    name: 'Svezia',
+                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+                },
+                {
+                    url: './img/02.jpg',
+                    name: 'Svizzera',
+                    text: 'Lorem ipsum.'
+                },
+                {
+                    url: './img/03.jpg',
+                    name: 'Gran Bretagna',
+                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.'
+                },
+                {
+                    url: './img/04.jpg',
+                    name: 'Germania',
+                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.'
+                },
+                {
+                    url: './img/05.jpg',
+                    name: 'Paradise',
+                    text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.'
+                }
+            ],
+            active: 0,
+            clock:0,
 
-            {
-                items: 'img/01.jpg', 
-                title: 'Svezia', 
-                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+        }, 
+
+        // ----------------------------------------------
+
+        methods: {
+            
+            previous() {
+                if (this.active == 0) {
+                    this.active = this.items.length - 1;
+                } else {
+                    this.active--;
+                }
+
             },
-        
-            {
-                items: 'img/02.jpg',
-                title: 'Svizzera',
-                text: 'Lorem ipsum',
+            next() {
+                if (this.active == this.items.length -1) {
+                    this.active = 0;
+                } else {
+                    this.active++;
+                }
+                
             },
-        
-            {
-                items: 'img/03.jpg',
-                title: 'Gran Bretagna',
-                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+
+            gotoSlide(index) {
+                this.active = index;
             },
-        
-            {
-                items: 'img/04.jpg',
-                title: 'Germania',
-                text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-            },
-        
-            {
-                items: 'img/05.jpg',
-                title: 'Paradise',
-                text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+
+            autoplay() {
+                this.clock = setInterval(() => {
+                    this.next();
+                },3000);
+                },
+
+            stopPlay() {
+                clearInterval(this.clock);
+                this.clock = null;
             }
-
-        ],
-
-    }
-)
+            }
+        }
+    
+);
 
 // ----------------------------------------------
+
+
 
